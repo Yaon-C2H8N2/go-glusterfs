603576bc1cffe1f588a66a815d8b921aa2f7d509
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-05-27 20:56:23 +0200

    feat 🖥️: added volume stop and delete

--- pkg/volume/volume.go
+++ pkg/volume/volume.go
@@ -2,6 +2,7 @@ package volume
 
 import (
 	"bytes"
+	"fmt"
 	"go-glusterfs.yaon.fr/pkg/brick"
 	"os/exec"
 )
@@ -14,9 +15,36 @@ type Volume struct {
 }
 
 func (v Volume) Start() error {
-	cmd := exec.Command("gluster", "volume", "start", v.Name)
+	cmd := exec.Command("gluster", "volume", "start", v.Name, "force")
 	out := bytes.Buffer{}
 	cmd.Stdout = &out
 	err := cmd.Run()
+	return err
+}
+
+func (v Volume) Stop() error {
+	cmd := exec.Command("gluster", "volume", "stop", v.Name)
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+	stdin, _ := cmd.StdinPipe()
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// Stop needs confirmation, no way to force it
+	if _, err := stdin.Write([]byte("y\n")); err != nil {
+		return err
+	}
+	stdin.Close()
+
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("%v: %s", err, stderr.String())
+	}
+
 	return err
 }
