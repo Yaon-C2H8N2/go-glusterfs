e5bc2618ae235b6983cc5214845a0fccc17ff6c3
Yoan Dusoleil <y.dusoleil@outlook.com>
2024-06-24 20:38:49 +0200

    feat 🖥: added event-driven functions

--- test/volume_test.go
+++ test/volume_test.go
@@ -2,9 +2,11 @@ package test
 
 import (
 	"github.com/Yaon-C2H8N2/go-glusterfs/pkg/brick"
+	"github.com/Yaon-C2H8N2/go-glusterfs/pkg/eventListener"
 	"github.com/Yaon-C2H8N2/go-glusterfs/pkg/peer"
 	"github.com/Yaon-C2H8N2/go-glusterfs/pkg/volume"
 	"testing"
+	"time"
 )
 
 func TestCreateVolume(t *testing.T) {
@@ -66,5 +68,85 @@ func TestDeleteWrongVolume(t *testing.T) {
 	err := volume.DeleteVolume("wrongvol")
 	if err == nil {
 		t.Fatalf("Deleting a non-existing volume should return an error")
+	}
+}
+
+func TestVolumeEvents(t *testing.T) {
+	volCreated := false
+	volStarted := false
+	volStopped := false
+	volDeleted := false
+
+	volUpdateHandler := func(event eventListener.VolumeEvent) {
+		if event.Type == eventListener.VOLUME_START {
+			volStarted = true
+		}
+		if event.Type == eventListener.VOLUME_STOP {
+			volStopped = true
+		}
+		if event.Type == eventListener.VOLUME_CREATE {
+			volCreated = true
+		}
+		if event.Type == eventListener.VOLUME_DELETE {
+			volDeleted = true
+		}
+	}
+
+	listener := eventListener.Default()
+	listener.SetPollingTimeout(100)
+	listener.OnVolumeUpdate = volUpdateHandler
+	err := listener.Start()
+	if err != nil {
+		t.Fatalf("Failed to start event listener: %s", err)
+	}
+
+	peers, err := peer.ListPeers()
+	if err != nil {
+		t.Fatalf("Failed to list peers: %s", err)
+	}
+	var bricks []brick.Brick
+	for _, p := range peers {
+		b := brick.Brick{Peer: p, Path: "/mnt/brick1/brick"}
+		bricks = append(bricks, b)
+	}
+	_, err = volume.CreateReplicatedVolume("testvol", bricks)
+	if err != nil {
+		t.Fatalf("Failed to create volume: %s", err)
+	}
+	// Wait for volume create event
+	time.Sleep(200 * time.Millisecond)
+	if !volCreated {
+		t.Fatalf("Volume create event not received")
+	}
+
+	v, err := volume.GetVolume("testvol")
+	if err != nil {
+		t.Fatalf("Failed to get volume: %s", err)
+	}
+	err = v.Start()
+	if err != nil {
+		t.Fatalf("Failed to start volume: %s", err)
+	}
+	err = v.Stop()
+	if err != nil {
+		t.Fatalf("Failed to stop volume: %s", err)
+	}
+	// Wait for volume events
+	time.Sleep(200 * time.Millisecond)
+	if !volStarted || !volStopped {
+		t.Fatalf("Volume start/stop events not received")
+	}
+	err = volume.DeleteVolume("testvol")
+	if err != nil {
+		t.Fatalf("Failed to delete volume: %s", err)
+	}
+	// Wait for volume deletion event
+	time.Sleep(200 * time.Millisecond)
+	if !volDeleted {
+		t.Fatalf("Volume delete event not received")
+	}
+	err = listener.Stop()
+	if err != nil {
+		t.Fatalf("Failed to stop event listener: %s", err)
 	}
 }
